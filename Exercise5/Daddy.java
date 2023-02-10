121877f1ef0f371df5a09234861cd0a81c1546f6
Aguilar, Paul Andrei A <119924415+MomoTzuZaki@users.noreply.github.com>
2023-02-10 14:24:34 +0800

    Add files via upload

---
+++ Exercise5/Daddy.java
@@ -0,0 +1,9 @@
+public class Daddy {
+
+    protected String surname = "Aguilar";
+
+    protected void showSurname() {
+        System.out.println("My family name is: "+ surname);
+    }
+    
+}
---
+++ Exercise5/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+private String Name = "Agular";
+
+public String getname(){
+    return this.Name;
+}
+
+public void setname(String personName){
+    this.Name = personName;
+    }
+}
